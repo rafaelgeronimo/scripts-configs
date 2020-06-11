cadef031fd2b14ede0bd7d5439517a07728008fd
Rafael Gerônimo <rafaelgeronimo@outlook.com>
2020-06-10 21:02:29 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -51,6 +51,6 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax/highlighting
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 echo "NÃO SE ESQUEÇA DE INSTALAR A FONTE FURAMONO DO SITE DO GOOGLE FONTS"
